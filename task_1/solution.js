2b4b7b932b6e74184f981cd39c4f9ad282b64ff2
Aleksandra <drakosha996699@mail.ru>
2022-04-07 22:58:00 +0300

    вервися 4

--- task_1/solution.js
+++ task_1/solution.js
@@ -57,7 +57,7 @@ function addInCartSumChange(sum, delta) {
     // Задание №1.3. Увеличение суммы в корзине
 
     let newSum = oldSum + difference;
-    let newSumText = newSum + "р";
+    let newSumText = newSum + "₽";
     newSumText = `${newSum} ₽`;
 
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
