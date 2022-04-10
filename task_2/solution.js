5502ed3982cd1d9058de8859c6ae5ff7b1c68773
Aleksandra <drakosha996699@mail.ru>
2022-04-10 22:16:12 +0300

    v15

--- task_2/solution.js
+++ task_2/solution.js
@@ -24,7 +24,7 @@ function calcShipping(sum, min, shipping) {
 
     // Конец решения задания №2.1.
 
-    return shippingSum;
+    return shippingPrice;
 }
 
 function calcDiscount(sum, min, discount) {
